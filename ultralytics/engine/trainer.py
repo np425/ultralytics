fa03334430965c21f551ee5304e74cd28a4038c4
np <email>
2025-08-21 21:13:20 +0300

    Updated save_period to track best.pt model

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -583,8 +583,9 @@ class BaseTrainer:
         self.last.write_bytes(serialized_ckpt)  # save last.pt
         if self.best_fitness == self.fitness:
             self.best.write_bytes(serialized_ckpt)  # save best.pt
-        if (self.save_period > 0) and (self.epoch % self.save_period == 0):
-            (self.wdir / f"epoch{self.epoch}.pt").write_bytes(serialized_ckpt)  # save epoch, i.e. 'epoch3.pt'
+            if (self.save_period > 0):
+                block = (self.epoch // self.save_period) * self.save_period
+                (self.wdir / f"epoch{block}_best.pt").write_bytes(serialized_ckpt)
         # if self.args.close_mosaic and self.epoch == (self.epochs - self.args.close_mosaic - 1):
         #    (self.wdir / "last_mosaic.pt").write_bytes(serialized_ckpt)  # save mosaic checkpoint
 
